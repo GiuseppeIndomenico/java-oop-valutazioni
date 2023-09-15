3d10b4375c0217b5d5c72c3161fa2da786305af7
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-09-15 17:49:33 +0200

    aggiunto array di 10 studenti

--- src/org/lessons/java/valutazioni/Main.java
+++ src/org/lessons/java/valutazioni/Main.java
@@ -1,20 +1,37 @@
 package org.lessons.java.valutazioni;
 
+
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-
         Random rnd = new Random();
+        
+        Studente[] stu = new Studente[10]; 
+        
+       int promossoCount=0;
+       
+        for (int i = 0; i < stu.length; i++) {
+        	int id = i + 1;
+        	float voto = Math.round(rnd.nextFloat(5)  * 10.0f) / 10.0f;
 
-      
-        float voto = Math.round(rnd.nextFloat() * 50) / 10.0f;
+            int assenze = rnd.nextInt(100);
+            stu[i] = new Studente(id, assenze, voto);
+        }
 
-        int assenze = rnd.nextInt(100);
+ 
+        for (int i = 0; i < stu.length; i++) {
+            Studente studente = stu[i];
+            studente.Print();
+           
+            if(studente.promosso) {
+            	promossoCount++;
+            }
+            System.out.println("\n-----------------\n");
+        }
+        
+        System.out.println("Ci sono " + promossoCount + " studenti promossi");
 
-        Studente mario = new Studente(1, assenze, voto);
-
-        mario.Print();
     }
 }
